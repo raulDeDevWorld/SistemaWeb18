f78570e951cbf153fe302eaf6b0b7edf32584e2b
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2023-04-24 21:28:28 -0400

    Update pdf.jsx
    Cc

--- components/pdf.jsx
+++ components/pdf.jsx
@@ -198,7 +198,7 @@ const PDFView = ({click}) => {
     console.log(pdfData)
 
     return (
-        <div style={{  textAlign: 'center',  width: '100%', background: 'red'}}>
+        <div style={{  textAlign: 'center',  width: '40%', background: 'red'}}>
             {isCliente && <PDFDownloadLink document={
                 <Document>
                     <Page style={styles.body} size="A4" fixed >
